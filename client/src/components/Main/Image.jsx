7861071a1c2a77ef2adaa5c3ed8df344b109d2f7
Mudit Bisht <3145159Noobi7uke@users.noreply.github.com>
2020-10-26 22:53:49 +0530

    Image preview

--- client/src/components/Main/Image.jsx
+++ client/src/components/Main/Image.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Image = function(){
+  const [image, setImage] = useState(null);
 
-    return (
-    <form method = "POST" action = "/upload/image" enctype="multipart/form-data" >  
-      <input type = "file" name = "image" />
-      <button type = "submit">Upload</button>
+  function handleChange(event) {
+    setImage(URL.createObjectURL(event.target.files[0]));
+  }
+
+  return (
+    <form method="POST" action="/upload/image" enctype="multipart/form-data" >
+      {image && <img src={image} alt="" width="300px" height="300px" />}
+      <input type="file" name="image" onChange={handleChange} />
+      <button type="submit">Upload</button>
     </form>
-
-    )
+  );
 }
 
 export default Image;
