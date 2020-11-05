804d5d77e0aa11487889912997d98692aa048a5d
MR.AV <akashvermaa0208@gmail.com>
2020-11-05 20:21:33 +0530

    change in get-images

--- server/Users/Images/handlers/getImages.js
+++ server/Users/Images/handlers/getImages.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const CLIENT_URL = "http://localhost:3000";
-const User = require("../../model").User;
 const Image = require("../../model").Image;
 const splitArray = require("../../routes/splitArray")
 router.get("/", (req, res) => {
-
-
 
     Image.find({}, (err, images) => {
         if (err) {
@@ -20,21 +17,6 @@ router.get("/", (req, res) => {
             res.send(ans);
         }
     });
-
-//    User.find({}, 'image', (err, images) => {
-//        if (err) {
-//            console.error("Some error occured ", err);
-//            res.status(504);
-//            res.send("Some error occured");
-//        } else {
-//            console.log(images);
-//            ans = splitArray(images);
-//            console.log(ans);
-//            res.send(ans);
-//        }
-//    });
-
-
 });
 
 module.exports = router
--- server/Users/routes/splitArray.js
+++ server/Users/routes/splitArray.js
@@ -1,11 +1,9 @@
 function splitArray(images){
 
     res = []
-    images.forEach(element => {
-        element.image.forEach(post => {
-            res.push({url : post.url, caption : post.caption, likes : post.likes.length, imageId: post._id, userId : element._id, comments : post.comments.length})
+    images.forEach(post => {
+            res.push({url : post.url, caption : post.caption, likes : post.likes.length, imageId: post._id, userId : post.userId, comments : post.comments.length})
         })
-    });
     return res;
 }
 
