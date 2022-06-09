196292c774831790d6cec6c3037578200e612c80
Akash Verma <49374276+MR-AV@users.noreply.github.com>
2022-06-09 17:44:16 +0530

    Update key.js

--- server/config/key.js
+++ server/config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
-    googleClientID: "161810578016-42u63lsuofcfdt4omvp260v5u2h4i419.apps.googleusercontent.com",
-    googleClientSecret: "EPbY9-dzHa3kXPWAIJ-tGOXM"
+    googleClientID: "",
+    googleClientSecret: ""
 };
